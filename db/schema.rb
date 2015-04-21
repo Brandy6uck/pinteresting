ea93f0d5be703160b4b58ea5e28da9061d2ae589
Dylan Chatterjee <dylan.chatterjee@gmail.com>
2015-04-21 15:33:12 -0400

    Add pins/user associations

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150325142200) do
+ActiveRecord::Schema.define(version: 20150325154226) do
 
   create_table "pins", force: :cascade do |t|
     t.string   "description"
